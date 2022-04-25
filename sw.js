c1af4c9a82fdfac0eccb555ed3f7833ec4b279e1
Roland Guelle <roland.guelle@sevenval.com>
2022-04-25 08:46:41 +0200

    update service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ https://css-tricks.com/serviceworker-for-offline/
 */
 
 "use strict";
-var version = 'v6';
+var version = 'v9';
 var offlineFundamentals = [
 ];
 self.addEventListener("install", function(event) {
